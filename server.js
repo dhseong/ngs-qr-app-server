2f19b1f8f744c180d9da704f2a83768b651de0c3
dhseong <dhseong81@gmail.com>
2021-02-23 16:56:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,17 @@ const app = express()
 const port = process.env.PORT || 5000;
 
 const cors = require("cors");
-app.use(cors());
-// app.use(
-// 	cors({
-// 		origin: [
-// 		  "http://localhost:3000",
-//       "http://skku-milab.ddns.net:3300",
-//       "https://ngs-qr.herokuapp.com"
-//     ],
-// 		credentials: true,
-// 	})
-// );
+// app.use(cors());
+app.use(
+	cors({
+		origin: [
+		  "http://localhost:3000",
+      "http://skku-milab.ddns.net:3300",
+      "https://ngs-qr.herokuapp.com"
+    ],
+		credentials: true,
+	})
+);
 
 mongoURI = 'mongodb+srv://ngsqr:ozbf5b8CdCU17xo5@cluster0.d75oo.mongodb.net/ngsqr?retryWrites=true&w=majority'
 
