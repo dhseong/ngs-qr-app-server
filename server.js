ce96de092dece8224600f750172eeb9d3f3552c7
dhseong <dhseong81@gmail.com>
2021-02-23 14:54:06 +0900

    Update server.js
    Initial commit

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ const app = express()
 const port = 5000
 
 const cors = require("cors");
-app.use(
-	cors({
-		origin: [
-		  "http://localhost:3000",
-      "http://skku-milab.ddns.net:3300",
-      "https://ngs-qr.herokuapp.com"
-    ],
-		credentials: true,
-	})
-);
+app.use(cors());
+// app.use(
+// 	cors({
+// 		origin: [
+// 		  "http://localhost:3000",
+//       "http://skku-milab.ddns.net:3300",
+//       "https://ngs-qr.herokuapp.com"
+//     ],
+// 		credentials: true,
+// 	})
+// );
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
